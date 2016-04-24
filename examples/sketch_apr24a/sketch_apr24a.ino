da6a0dfc9c79023c19a6af49a3840170dc5cef53
micchas <miccha@tlen.pl>
2016-04-24 03:56:30 +0200

    nowy przykład
    Przykład do pomocy przy pracy nad projektem

---
+++ examples/sketch_apr24a/sketch_apr24a.ino
@@ -0,0 +1,41 @@
+/*
+ * #include <RadioHead.h>
+#include <radio_config_Si4460.h>
+#include <RHCRC.h>
+#include <RHDatagram.h>
+#include <RHGenericDriver.h>
+#include <RHGenericSPI.h>
+#include <RHHardwareSPI.h>
+#include <RHMesh.h>
+#include <RHNRFSPIDriver.h>
+#include <RHReliableDatagram.h>
+#include <RHRouter.h>
+#include <RHSoftwareSPI.h>
+#include <RHSPIDriver.h>
+#include <RHTcpProtocol.h>
+#include <RH_ASK.h>
+#include <RH_CC110.h>
+#include <RH_MRF89.h>
+*/
+#include <RH_NRF24.h>
+/*
+ * 
+ */#include <RH_NRF51.h>
+#include <RH_NRF905.h>
+#include <RH_RF22.h>
+#include <RH_RF24.h>
+#include <RH_RF69.h>
+#include <RH_RF95.h>
+#include <RH_Serial.h>
+#include <RH_TCP.h>
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
